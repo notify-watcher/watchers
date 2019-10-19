cee40c2d31089a2eae47ab168d1a06d4f765df2e
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-19 00:15:27 -0300

    move unired-tag config to its own file

--- unired-tag/index.js
+++ unired-tag/index.js
@@ -1,25 +1,9 @@
 const {
-  constants: { ERRORS, TIMEFRAMES },
+  constants: { ERRORS },
 } = require('@notify-watcher/core');
+const config = require('./config');
 
 const UNIRED_URL = 'https://www.unired.cl';
-
-const config = {
-  name: 'unired-tag-total',
-  displayName: 'Unired Tag Total',
-  description: 'Watch your tag ballot using Unired',
-  auth: true,
-  libs: ['puppeteer'],
-  timeframe: {
-    type: TIMEFRAMES.minute,
-  },
-  notificationTypes: {
-    updatedBallot: {
-      key: 'updatedBallot',
-      description: 'Your Tag ballot changed.',
-    },
-  },
-};
 
 async function waitAndClick(page, selector) {
   await page.waitForSelector(selector);
